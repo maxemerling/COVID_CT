4c54b11247e273e26fca612d173546cb2822c1bb
Max Emerling <memerling@berkeley.edu>
2020-04-03 16:25:03 -0700

    added bypass connections to squeezenet

--- keras_squeezenet.py
+++ keras_squeezenet.py
@@ -3,7 +3,7 @@ Adapted from https://github.com/rcmalli/keras-squeezenet/blob/master/keras_squee
 """
 
 from keras.models import Model
-from keras.layers import Input, Convolution2D, Activation, MaxPooling2D, GlobalAveragePooling2D, Dropout, concatenate
+from keras.layers import Input, Convolution2D, Activation, MaxPooling2D, GlobalAveragePooling2D, Dropout, concatenate, add
 from keras.utils import get_file
 
 WEIGHTS_PATH_NO_TOP = "https://github.com/rcmalli/keras-squeezenet/releases/download/v1.0/squeezenet_weights_tf_dim_ordering_tf_kernels_notop.h5"
@@ -23,25 +23,71 @@ def fire_module(x, fire_id, squeeze=16, expand=64):
     
     return x
 
-def SqueezeNet(input_shape, weights='imagenet'):
+def simple_add(from_layer, to_layer):
+    return add([from_layer, to_layer])
+
+def channels(layer):
+    return layer.shape[3]
+
+def complex_add(complex_id, from_layer, to_layer):
+    complex_id = 'complex' + str(complex_id)
+    from_layer = Convolution2D(channels(to_layer), (1, 1), padding='same', name=complex_id)(from_layer)
+    from_layer = Activation('relu', name='relu_' + complex_id)(from_layer)
+    return simple_add(from_layer, to_layer)
+
+
+"""
+Note: the simple and complex bypass connections feed into each other unless separated by a pooling layer.
+"""
+def SqueezeNet(input_shape, weights='imagenet', bypass=None):
+    
+    if weights not in {None, 'imagenet', 'base_model'}:
+        raise ValueError('Unknown `weights` argument')
+    
+    if bypass == 'simple':
+        simple = True
+        complex = False
+    elif bypass == 'complex':
+        simple = complex = True
+    elif bypass == None:
+        simple = complex = False
+    else:
+        raise ValueError('Unknown `bypass` argument')
+    
     inputs = Input(shape=input_shape)
     
     x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(inputs)
     x = Activation('relu', name='relu_conv1')(x)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
+    c = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
 
-    x = fire_module(x, fire_id=2, squeeze=16, expand=64)
-    x = fire_module(x, fire_id=3, squeeze=16, expand=64)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
-
-    x = fire_module(x, fire_id=4, squeeze=32, expand=128)
-    x = fire_module(x, fire_id=5, squeeze=32, expand=128)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
-
-    x = fire_module(x, fire_id=6, squeeze=48, expand=192)
-    x = fire_module(x, fire_id=7, squeeze=48, expand=192)
-    x = fire_module(x, fire_id=8, squeeze=64, expand=256)
-    x = fire_module(x, fire_id=9, squeeze=64, expand=256)
+    s = fire_module(c, fire_id=2, squeeze=16, expand=64)
+    if complex:
+        s = complex_add(1, c, s)
+    c = fire_module(s, fire_id=3, squeeze=16, expand=64)
+    if simple:
+        c = simple_add(s, c)
+    x = fire_module(c, fire_id=4, squeeze=32, expand=128)
+    if complex:
+        x = complex_add(2, c, x)
+    s = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
+    
+    c = fire_module(s, fire_id=5, squeeze=32, expand=128)
+    if simple:
+        c = simple_add(s, c)
+    s = fire_module(c, fire_id=6, squeeze=48, expand=192)
+    if complex:
+        s = complex_add(3, c, s)
+    c = fire_module(s, fire_id=7, squeeze=48, expand=192)
+    if simple:
+        c = simple_add(s, c)
+    x = fire_module(c, fire_id=8, squeeze=64, expand=256)
+    if complex:
+        x = complex_add(4, c, x)
+    s = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
+    
+    x = fire_module(s, fire_id=9, squeeze=64, expand=256)
+    if simple:
+        x = simple_add(s, x)
 
     model = Model(inputs, x, name='squeezenet')
     
@@ -52,8 +98,6 @@ def SqueezeNet(input_shape, weights='imagenet'):
                                     WEIGHTS_PATH_NO_TOP,
                                     cache_subdir='models')
         model.load_weights(weights_path)
-    elif weights is not None:
-        raise ValueError('Unknown `weights` argument')
     
     
     return model
